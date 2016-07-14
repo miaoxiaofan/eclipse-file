dae8fc8ee1346c31ed55b198ba9354fde1ff687d
miaoxiaofan <1429425727@qq.com>
2016-07-14 21:23:03 +0800

    测试44

---
+++ test44/src/test44/yyy.java
@@ -0,0 +1,14 @@
+package test44;
+
+public class yyy {
+
+	public yyy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("gjhjvhj");
+	}
+
+}
