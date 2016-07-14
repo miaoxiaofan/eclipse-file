88b86832f24649d7605c843e1a8bd1be1e2f6936
miaoxiaofan <1429425727@qq.com>
2016-07-14 21:37:19 +0800

    titgu哈哈哈

---
+++ test55/src/test55/ccc.java
@@ -0,0 +1,14 @@
+package test55;
+
+public class ccc {
+
+	public ccc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
