f22d38d99cbd350064d647319507e537b74aeab7
miaoxiaofan <1429425727@qq.com>
2016-07-14 21:44:22 +0800

    正式测试

---
+++ test66/src/test66/dddss.java
@@ -0,0 +1,14 @@
+package test66;
+
+public class dddss {
+
+	public dddss() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
